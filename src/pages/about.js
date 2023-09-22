9884b57387119483a64a04ab3b7fce4a0efee94b
ch4nt <heusser@protonmail.com>
2023-09-22 11:37:18 -0400

    added some pics

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 // Step 1: Import React
 import * as React from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 
@@ -8,6 +9,17 @@ const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
       <p>I work in Information Security and hope to help others grow their skills by sharing resources and knowledge.</p>
+      <p>I hold the following active certifications:</p>
+      <StaticImage
+        alt="BTL1 certified"
+        src="../images/btl1certified.png"
+        width="175"
+      />
+       <StaticImage
+        alt="CISSP certified"
+        src="../images/cisspcertified.png"
+        width="175"
+      />
     </Layout>
   )
 }
